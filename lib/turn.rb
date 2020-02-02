6088a385c8b33182afa504021d4129ced60e0111
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-02-02 16:34:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ def turn(board)
       puts "Please enter 1-9:"
       input= gets.strip
       index=input_to_index(input)
-      if valid_move?(board,index)==false
+      if valid_move?(board,index) == false
         puts "Please enter 1-9:"
         input= gets.strip
         index=input_to_index(input)
